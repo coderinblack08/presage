23d07dc6cd73f0268f786bb671b8199c44dd36b7
coderinblack <kevinlu.email@gmail.com>
2021-04-20 20:40:25 -0700

    Add publisher info to sql query and error handling

--- web/pages/api/articles.ts
+++ web/pages/api/articles.ts
@@ -8,32 +8,36 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   });
   await client.connect();
 
-  const result = await client.query(
-    `
-    select p.name, 
-    array_agg(json_build_object(
-      'id', a.id,
-      'category', a.category,
-      'title', a.title,
-      'description', a.description,
-      'date', a.date,
-      'url', a.url,
-      'image', a.image
-    ))
-    from publishers p
-    join lateral (
-      select id, publisher, category, title, description, date, url, image
-      from articles
-      where publisher = p.name
-      -- inner join publishers p on publisher = p.name
-      limit $1
-    ) a on a.publisher = p.name
-    group by p.name;
-  `,
-    [limit]
-  );
-
-  res
-    .status(200)
-    .json(result.rows.map((x) => ({ name: x.name, articles: x.array_agg })));
+  try {
+    const result = await client.query(
+      `
+      select p.name, 
+      array_agg(json_build_object(
+        'id', a.id,
+        'category', a.category,
+        'title', a.title,
+        'publisher_name', p.name,
+        'publisher_profile_picture', p.profile_picture,
+        'description', a.description,
+        'date', a.date,
+        'url', a.url,
+        'image', a.image
+      ))
+      from publishers p
+      join lateral (
+        select id, publisher, category, a.description, title, date, url, image
+        from articles a
+        where publisher = p.name
+        limit $1
+      ) a on a.publisher = p.name
+      group by p.name;
+      `,
+      [limit]
+    );
+    res
+      .status(200)
+      .json(result.rows.map((x) => ({ name: x.name, articles: x.array_agg })));
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
