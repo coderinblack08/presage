80a9845bb9d42d1220f6f775b8ef0a981c350fba
coderinblack <kevinlu.email@gmail.com>
2021-04-23 16:37:06 -0700

    Add gitignore to root

--- scrapper/lib/rss.ts
+++ scrapper/lib/rss.ts
@@ -1,6 +1,7 @@
-export const rss: Record<string, { top: string }> = {
+export const rss: Record<string, { top?: string; us?: string }> = {
   CNN: {
     top: "http://rss.cnn.com/rss/edition.rss",
+    us: "http://rss.cnn.com/rss/cnn_us.rss",
   },
   "Fox News": {
     top: "http://feeds.foxnews.com/foxnews/latest",
