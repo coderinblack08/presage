612d62a94bce328807341af3893bf1cc4d790703
coderinblack <kevinlu.email@gmail.com>
2021-06-07 12:44:51 -0700

    feat(web): Setup tailwind.css

---
+++ packages/web/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
