567cfe586f009387679ca1ac5416f46427c89d90
coderinblack <kevinlu.email@gmail.com>
2021-06-07 14:22:31 -0700

    feat(web): Add button and spinner components

---
+++ packages/web/components/Button.tsx
@@ -0,0 +1,58 @@
+import React, {
+  ButtonHTMLAttributes,
+  DetailedHTMLProps,
+  ReactNode,
+} from "react";
+import { Spinner } from "./Spinner";
+
+const ButtonTheme = {
+  size: {
+    md: "py-2 px-6 rounded-lg",
+    sm: "px-5 py-1.5 rounded-lg",
+  },
+  color: {
+    primary:
+      "text-white bg-primary hover:bg-faint-primary disabled:text-white-primary disabled:bg-faint-primary",
+    gray: "text-white bg-darker-gray hover:bg-dark-gray disabled:bg-dark-gray disabled:text-light-gray",
+  },
+};
+
+export type ButtonProps = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> & {
+  size?: keyof typeof ButtonTheme.size;
+  color?: keyof typeof ButtonTheme.color;
+  icon?: ReactNode;
+  transition?: boolean;
+  loading?: boolean;
+};
+
+export const Button: React.FC<ButtonProps> = ({
+  size = "md",
+  color = "primary",
+  icon,
+  loading,
+  className,
+  children,
+  ...props
+}) => {
+  return (
+    <button
+      className={`${ButtonTheme.size[size]} ${ButtonTheme.color[color]} focus:outline-none focus-visible:ring inline-flex items-center justify-center transition shadow-sm ${className}`}
+      {...props}
+    >
+      <span className={loading ? "opacity-0" : `flex items-center`}>
+        {icon && <span className={children ? "mr-2" : ""}>{icon}</span>}
+        <p className={`font-bold ${size === "sm" ? "small" : ""}`}>
+          {children}
+        </p>
+      </span>
+      {loading ? (
+        <span className="absolute">
+          <Spinner />
+        </span>
+      ) : null}
+    </button>
+  );
+};
---
+++ packages/web/components/Spinner.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface SpinnerProps {}
+
+export const Spinner: React.FC<SpinnerProps> = ({}) => {
+  return <div className="spinner" />;
+};
