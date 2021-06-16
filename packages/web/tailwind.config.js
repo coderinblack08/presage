76c4ea09607b7a5c3a956f2d5df4776861dfbc5d
coderinblack <kevinlu.email@gmail.com>
2021-06-15 22:41:26 -0700

    feat(web): Add tailwind.css

---
+++ packages/web/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ["./modules/**/*.tsx", "./pages/**/*.tsx", "./components/**/*.tsx"],
+  mode: "jit",
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
