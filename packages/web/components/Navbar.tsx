f42eb6053a30ffaf7a5ac93916d43a348eda03c7
coderinblack <kevinlu.email@gmail.com>
2021-06-28 18:45:04 -0700

    feat(web): User avatar in navbar

--- packages/web/components/Navbar.tsx
+++ packages/web/components/Navbar.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { Paper, Upload, Wallet } from "react-iconly";
+import { Paper, TicketStar, Upload, Wallet } from "react-iconly";
+import { useQuery } from "react-query";
+import { User } from "../lib/types";
 import logo from "../public/static/logo.png";
 import { Button } from "./Button";
 
 interface NavbarProps {}
 
 export const Navbar: React.FC<NavbarProps> = ({}) => {
+  const { data: me } = useQuery<User>("/me");
+
   return (
     <nav className="max-w-8xl mx-auto flex items-center justify-between py-6 px-8">
       <Link href="/">
@@ -16,7 +20,7 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
           <h4 className="text-white">Presage</h4>
         </a>
       </Link>
-      <div className="hidden md:flex items-center space-x-14">
+      <div className="hidden md:flex items-center space-x-10 lg:space-x-14">
         <a className="flex items-center">
           <div className="mr-3">
             <Paper set="bulk" />
@@ -29,13 +33,36 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
           </div>
           Pricing
         </a>
-        <a className="flex items-center">
-          <div className="mr-3">
-            <Upload set="bulk" />
+        <Link href="/publish">
+          <a className="flex items-center">
+            <div className="mr-3">
+              <Upload set="bulk" />
+            </div>
+            Publish
+          </a>
+        </Link>
+        {me ? (
+          <div className="flex item-center space-x-6">
+            <div className="flex items-center flex-grow-0">
+              <Button
+                color="gray"
+                icon={<TicketStar set="bulk" />}
+                className="bg-gray-600 border border-gray-500 hover:bg-gray-500"
+              >
+                12 <span className="text-gray-200">Referrals</span>
+              </Button>
+            </div>
+            <img
+              src={me.profilePicture}
+              alt={me.displayName}
+              className="w-12 h-12 rounded-full object-cover"
+            />
           </div>
-          Upload
-        </a>
-        <Button>Login</Button>
+        ) : (
+          <a href="http://localhost:4000/auth/google">
+            <Button>Login</Button>
+          </a>
+        )}
       </div>
     </nav>
   );
--- packages/web/lib/types.ts
+++ packages/web/lib/types.ts
@@ -8,39 +8,3 @@ export interface User {
   createdAt: Date;
   updatedAt: Date;
 }
-
-export interface Echo {
-  id: string;
-  title: string;
-  description?: string;
-  audio: string;
-  thumbnail?: string;
-  duration: number;
-  userId: string;
-  categoryId?: string;
-  user: User;
-  category?: Category;
-  createdAt: Date;
-  updatedAt: Date;
-}
-
-export interface Category {
-  id: string;
-  name: string;
-  echoCount: number;
-}
-
-export interface Radio {
-  id: string;
-  prompt: string;
-  description?: string;
-  closed: boolean;
-  responses: number;
-  userId: string;
-  user: User;
-  echos?: Echo[];
-  categoryId?: string;
-  category?: Category;
-  createdAt: Date;
-  updatedAt: Date;
-}
