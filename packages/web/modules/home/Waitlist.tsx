47ecadc26ce83cc9d5a27a7036a341252a9bb9a2
coderinblack <kevinlu.email@gmail.com>
2021-07-15 15:50:43 -0700

    feat(web): change toast message

--- packages/web/modules/home/Waitlist.tsx
+++ packages/web/modules/home/Waitlist.tsx
@@ -5,43 +5,48 @@ import { Input } from "../../components/Input";
 
 interface WaitlistProps {}
 
-const notify = () => toast.success("Email added!");
+const notify = () => toast.success("Success! Thanks for joining the Waitlist.");
 
 export const Waitlist: React.FC<WaitlistProps> = ({}) => {
   const [email, setEmail] = useState("");
   const [loading, setLoading] = useState(false);
 
   return (
-    <form
-      className="relative flex items-center space-x-2 mt-5 sm:mt-8 max-w-md"
-      onSubmit={async (e) => {
-        e.preventDefault();
-        setLoading(true);
-        await fetch("/api/waitlist", {
-          method: "POST",
-          body: JSON.stringify({ email }),
-        });
-        setLoading(false);
-        notify();
-        setEmail("");
-      }}
-    >
-      <Input
-        placeholder="Email Address"
-        type="email"
-        className="!py-3 pr-44"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-        required
-      />
-      <Button
-        type="submit"
-        className="absolute right-1 flex-shrink-0"
-        loading={loading}
+    <>
+      <form
+        className="relative flex items-center space-x-2 mt-5 sm:mt-8 max-w-md"
+        onSubmit={async (e) => {
+          e.preventDefault();
+          setLoading(true);
+          await fetch("/api/waitlist", {
+            method: "POST",
+            body: JSON.stringify({ email }),
+          });
+          setLoading(false);
+          notify();
+          setEmail("");
+        }}
       >
-        Join Waitlist
-      </Button>
+        <Input
+          placeholder="Email Address"
+          type="email"
+          className="!py-3 pr-44"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          required
+        />
+        <Button
+          type="submit"
+          className="absolute right-1 flex-shrink-0"
+          loading={loading}
+        >
+          Join Waitlist
+        </Button>
+      </form>
       <Toaster position="top-center" gutter={8} />
-    </form>
+      <button type="button" onClick={notify}>
+        popup
+      </button>
+    </>
   );
 };
