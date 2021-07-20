7f084c8dae17eb581c46188e89000d0ceb52fbe7
coderinblack <kevinlu.email@gmail.com>
2021-07-19 21:16:13 -0700

    feat(web): user page

--- packages/web/pages/u/[username].tsx
+++ packages/web/pages/u/[username].tsx
@@ -1,18 +1,21 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { useRouter } from "next/router";
 import React from "react";
+import { MdEdit } from "react-icons/md";
 import { QueryClient, useQuery } from "react-query";
 import { dehydrate } from "react-query/hydration";
 import { ArticleNavbar } from "../../components/ArticleNavbar";
 import { Button } from "../../components/Button";
-import { Input } from "../../components/Input";
 import { ssrFetcher } from "../../lib/fetcher";
 import { useSSRQuery } from "../../lib/hooks/useSSRQuery";
 import { Article, Journal, User } from "../../lib/types";
+import { ArticleCard } from "../../modules/article/ArticleCard";
+import { FollowButton } from "../../modules/user/FollowButton";
 
 const UserPage: React.FC<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > = ({ username }) => {
+  const { data: me } = useQuery<User>(`/me`);
   const { data: user } = useSSRQuery<User>(`/user/${username}`);
   const { data: journals } = useQuery<Journal[]>(`/journals/${user.id}`);
   const router = useRouter();
@@ -24,23 +27,34 @@ const UserPage: React.FC<
   return (
     <div>
       <ArticleNavbar user={user} lightGray />
-      <header className="bg-gray-50 pt-4 pb-16 md:pb-20 md:pt-8 border-b">
+      <header className="bg-gray-50 pt-4 md:pt-6 pb-12 md:pb-16 border-b">
         <div className="flex items-start space-x-6 md:space-x-8 max-w-4xl mx-auto px-5 md:px-8">
           <img
-            src={user?.profilePicture}
-            alt={user?.displayName}
+            src={user.profilePicture}
+            alt={user.displayName}
             className="w-16 h-16 rounded-full"
           />
           <div className="flex-shrink-0 w-full">
-            <h4>{user?.displayName}</h4>
-            <p className="text-gray-600">@{user?.username}</p>
-            {user?.bio && (
-              <p className="mt-4 text-gray-500">
-                Founder of presage; blogging about the startup process, math,
-                software, and more.
-              </p>
-            )}
-            <form className="relative flex items-center mt-5 max-w-md">
+            <h4>{user.displayName}</h4>
+            <p className="text-gray-600">@{user.username}</p>
+            <p className="text-gray-500 mt-2">
+              {user.followersCount} followers · {user.followingCount} following
+            </p>
+            {user.bio && <p className="mt-4 text-gray-500">{user.bio}</p>}
+            <div className="mt-5">
+              {me?.id === user.id ? (
+                <Button
+                  size="small"
+                  icon={<MdEdit className="w-5 h-5" />}
+                  rounded
+                >
+                  Edit Profile
+                </Button>
+              ) : (
+                <FollowButton username={user.username} />
+              )}
+            </div>
+            {/* <form className="relative flex items-center mt-5 max-w-md">
               <Input
                 placeholder="Email Address"
                 type="email"
@@ -55,8 +69,8 @@ const UserPage: React.FC<
               </Button>
             </form>
             <label className="text-gray-400 small mt-2 block">
-              Get notified when @{user?.username} posts
-            </label>
+              Get notified when @{user.username} posts
+            </label> */}
           </div>
         </div>
       </header>
@@ -66,7 +80,7 @@ const UserPage: React.FC<
           <p className="text-gray-500 mt-1 text-sm">
             Kevin Lu’s personal journal dedicated to blogging
           </p>
-          <div className="my-5 max-w-4xl inline-flex items-center space-x-2">
+          <div className="mt-6 mb-4 max-w-4xl inline-flex items-center space-x-2">
             <Button
               size="small"
               color={journalId === undefined ? "white" : "transparent"}
@@ -108,6 +122,16 @@ const UserPage: React.FC<
                   {journal.name}
                 </p>
               </Button>
+            ))}
+          </div>
+          <div className="grid grid-cols-1 gap-4">
+            {articles?.length === 0 ? (
+              <div className="text-gray-500 p-6 rounded-lg bg-white shadow">
+                This journal is empty, check on it later.
+              </div>
+            ) : null}
+            {articles?.map((article) => (
+              <ArticleCard key={article.id} article={article} />
             ))}
           </div>
         </div>
