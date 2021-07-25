be92e7856dcac9761614363c1813571fd2a2380a
coderinblack <kevinlu.email@gmail.com>
2021-07-25 16:12:57 -0700

    feat(api): remove rate limiting for dev

--- packages/api/src/lib/rateLimit.ts
+++ packages/api/src/lib/rateLimit.ts
@@ -1,15 +1,18 @@
 import rateLimit from "express-rate-limit";
+import { NextFunction } from "express-serve-static-core";
 import RedisStore from "rate-limit-redis";
-import { SECONDS_IN_HOUR } from "./constants";
+import { isDev, SECONDS_IN_HOUR } from "./constants";
 import { redis } from "./redis";
 
 export const limiter = (options: Partial<rateLimit.Options>) =>
-  rateLimit({
-    store: new RedisStore({
-      client: redis,
-      expiry: options.windowMs || 100,
-    }),
-    windowMs: SECONDS_IN_HOUR,
-    max: 100,
-    ...options,
-  });
+  isDev()
+    ? (_: any, __: any, next: NextFunction) => next()
+    : rateLimit({
+        store: new RedisStore({
+          client: redis,
+          expiry: options.windowMs || 100,
+        }),
+        windowMs: SECONDS_IN_HOUR,
+        max: 100,
+        ...options,
+      });
--- packages/web/pages/publish.tsx
+++ packages/web/pages/publish.tsx
@@ -1,20 +1,23 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import React from "react";
-import { QueryClient } from "react-query";
+import { QueryClient, useQuery } from "react-query";
 import { dehydrate } from "react-query/hydration";
 import { ssrFetcher } from "../lib/fetcher";
+import { Article } from "../lib/types";
 import { DraftSidebar } from "../modules/draft/DraftSidebar";
 
 const Publish: React.FC<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > = ({ draftId }) => {
+  const { data: draft } = useQuery<Article>(`/articles/draft/${draftId}`);
+
   return (
     <div className="flex items-start">
       <div className="w-full max-w-xs">
         <DraftSidebar />
       </div>
       <div className="w-full h-screen overflow-y-scroll bg-white">
-        <header className="p-6"></header>
+        <header className="p-6">{draft?.title}</header>
       </div>
     </div>
   );
