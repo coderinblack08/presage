61b84a841c58033cb55757264c10c964795347b2
coderinblack08 <kevinlu.email@gmail.com>
2021-08-05 20:39:33 -0700

    feat(web): responsive explore page

--- packages/web/modules/home/ExplorePage.tsx
+++ packages/web/modules/home/ExplorePage.tsx
@@ -21,11 +21,11 @@ export const ExplorePage: React.FC<ExplorePageProps> = ({}) => {
   return (
     <div className="py-5 md:py-8">
       <h4>Your Curated Feed</h4>
-      <p className="text-gray-600 mt-1">
+      <p className="text-gray-600 text-sm sm:text-base mt-1">
         Did you know? — Select your favorite tags to add them as filters
       </p>
-      <div className="flex items-center mt-8">
-        <div className="items-center relative mr-5">
+      <div className="flex flex-col-reverse sm:flex-row sm:items-center mt-6 sm:mt-8">
+        <div className="items-center relative mt-3 sm:mt-0 sm:mr-5">
           <Input
             onKeyDown={(e) => {
               if (e.key === "Enter") {
@@ -40,22 +40,22 @@ export const ExplorePage: React.FC<ExplorePageProps> = ({}) => {
             }}
             value={query}
             placeholder="Search"
-            className="w-72 pl-12"
+            className="w-full sm:w-72 pl-12"
           />
           <div className="h-full mx-4 flex items-center pointer-events-none absolute top-0 left-0 text-gray-500">
             <Search size="small" stroke="bold" />
           </div>
         </div>
-        <div className="h-8 border-r border-gray-300" />
-        <nav className="flex space-x-1.5 items-center ml-5">
+        <div className="hidden sm:block h-8 border-r border-gray-300" />
+        <nav className="flex space-x-1.5 items-center sm:ml-5">
           <Button color="white" size="small">
-            <span className="font-bold">Articles</span>
+            <span className="font-bold sm:text-base text-sm">Articles</span>
           </Button>
           <Button color="transparent" size="small">
-            <span className="text-gray-600">Journals</span>
+            <span className="text-gray-600 sm:text-base text-sm">Journals</span>
           </Button>
           <Button color="transparent" size="small">
-            <span className="text-gray-600">Users</span>
+            <span className="text-gray-600 sm:text-base text-sm">Users</span>
           </Button>
         </nav>
       </div>
