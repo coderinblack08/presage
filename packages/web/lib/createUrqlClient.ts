21a470e52e982a2c4400507177bf1bb014f2cd9d
coderinblack08 <kevinlu.email@gmail.com>
2021-08-16 13:25:14 -0700

    feat(web): forward cookies

--- packages/web/lib/createUrqlClient.ts
+++ packages/web/lib/createUrqlClient.ts
@@ -1,9 +1,18 @@
 import { dedupExchange, cacheExchange, fetchExchange } from "@urql/core";
+import { isServer } from "./constants";
 
-export const createUrqlClient = (ssrExchange: any) => ({
-  url: "http://localhost:4000/graphql",
-  fetchOptions: {
-    credentials: "include" as const,
-  },
-  exchanges: [dedupExchange, cacheExchange, ssrExchange, fetchExchange],
-});
+export const createUrqlClient = (ssrExchange: any, ctx: any) => {
+  let cookie = "";
+  if (isServer()) {
+    cookie = ctx?.req?.headers?.cookie;
+  }
+
+  return {
+    url: "http://localhost:4000/graphql",
+    fetchOptions: {
+      credentials: "include" as const,
+      headers: cookie ? { cookie } : undefined,
+    },
+    exchanges: [dedupExchange, cacheExchange, ssrExchange, fetchExchange],
+  };
+};
