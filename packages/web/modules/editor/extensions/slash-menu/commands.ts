b95c1fa283d05d235896511f08997079aa5c12bb
coderinblack08 <kevinlu.email@gmail.com>
2021-08-21 18:13:00 -0700

    feat(web): commands fts

--- packages/web/modules/editor/extensions/slash-menu/commands.ts
+++ packages/web/modules/editor/extensions/slash-menu/commands.ts
@@ -1,4 +1,5 @@
 import { Extension } from "@tiptap/core";
+import Fuse from "fuse.js";
 import { Editor, Range, ReactRenderer } from "@tiptap/react";
 import Suggestion, { SuggestionOptions } from "@tiptap/suggestion";
 import { Node as ProseMirrorNode } from "prosemirror-model";
@@ -37,44 +38,47 @@ export const Commands = Extension.create<CommandsOption>({
 
 const textItemHelper = (type: "heading" | "paragraph", level?: number) => ({
   title: type === "heading" ? `Heading ${level}` : "Paragraph",
-  description: "Big section heading",
+  description: level ? "Big section heading" : "Plain text",
+  shortcut: level ? `h${level}` : "p",
   command: ({ editor, range }: { editor: Editor; range: Range }) => {
     editor.chain().focus().deleteRange(range).setNode(type, { level }).run();
   },
 });
 
+const commands = [
+  textItemHelper("paragraph"),
+  textItemHelper("heading", 1),
+  textItemHelper("heading", 2),
+  textItemHelper("heading", 3),
+  {
+    title: "Ordered List",
+    description: "Create a list with numberings",
+    command: ({ editor, range }: { editor: Editor; range: Range }) => {
+      editor.chain().focus().deleteRange(range).toggleOrderedList().run();
+    },
+  },
+  {
+    title: "Bulleted List",
+    description: "Create a bulleted list",
+    command: ({ editor, range }: { editor: Editor; range: Range }) => {
+      editor.chain().focus().deleteRange(range).toggleBulletList().run();
+    },
+  },
+  {
+    title: "Quote",
+    description: "Create a quote",
+    command: ({ editor, range }: { editor: Editor; range: Range }) => {
+      editor.chain().focus().deleteRange(range).toggleBlockquote().run();
+    },
+  },
+];
+
+const fuse = new Fuse(commands, { keys: ["title", "description", "shortcut"] });
+
 export const SlashCommands = Commands.configure({
   suggestion: {
     items: (query) => {
-      return [
-        textItemHelper("paragraph"),
-        textItemHelper("heading", 1),
-        textItemHelper("heading", 2),
-        textItemHelper("heading", 3),
-        {
-          title: "Ordered List",
-          description: "Create a list with numberings",
-          command: ({ editor, range }: { editor: Editor; range: Range }) => {
-            editor.chain().focus().deleteRange(range).toggleOrderedList().run();
-          },
-        },
-        {
-          title: "Bulleted List",
-          description: "Create a bulleted list",
-          command: ({ editor, range }: { editor: Editor; range: Range }) => {
-            editor.chain().focus().deleteRange(range).toggleBulletList().run();
-          },
-        },
-        {
-          title: "Quote",
-          description: "Create a quote",
-          command: ({ editor, range }: { editor: Editor; range: Range }) => {
-            editor.chain().focus().deleteRange(range).toggleBlockquote().run();
-          },
-        },
-      ].filter((item) =>
-        item.title.toLowerCase().includes(query.toLowerCase())
-      );
+      return query ? fuse.search(query).map((x) => x.item) : commands;
     },
     render: () => {
       let component: ReactRenderer;
