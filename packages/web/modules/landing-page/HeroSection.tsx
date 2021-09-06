5053ad4a17958a21c20ef8b49459e58d9420a076
coderinblack08 <kevinlu.email@gmail.com>
2021-09-06 12:06:28 -0700

    feat(web): add landing page animation

--- packages/web/modules/landing-page/HeroSection.tsx
+++ packages/web/modules/landing-page/HeroSection.tsx
@@ -1,19 +1,23 @@
-import React from "react";
-import dashboard from "../../public/static/dashboard.png";
+import { motion, useTransform, useViewportScroll } from "framer-motion";
 import Image from "next/image";
+import React, { useEffect } from "react";
 import { useScreen } from "../../lib/useScreen";
 import { ListCheck } from "../../modules/landing-page/ListCheck";
 import { Waitlist } from "../../modules/landing-page/Waitlist";
+import dashboard from "../../public/static/dashboard-2.png";
 import { Navbar } from "./Navbar";
 interface HeroSectionProps {}
 
 export const HeroSection: React.FC<HeroSectionProps> = ({}) => {
   const { isSmallerThanTablet } = useScreen();
+  const { scrollY } = useViewportScroll();
+  const rotateX = useTransform(scrollY, [0, 500], [15, 0]);
+  const scale = useTransform(scrollY, [0, 500], [1, 1.1]);
 
   return (
     <header className="relative bg-gray-100">
       <Navbar />
-      <header className="relative z-10 flex flex-col items-center max-w-3xl mx-auto pt-8 pb-24 md:pt-16 md:pb-8 px-5">
+      <header className="relative z-10 flex flex-col items-center max-w-3xl mx-auto pt-8 pb-24 md:pt-16 md:pb-32 px-5">
         <h1 className="font-bold font-display text-3xl sm:text-4xl md:text-5xl xl:text-[3.5rem] text-center !leading-normal md:!leading-snug tracking-[-0.01em]">
           <span className="text-gray-500">Earn from publishing</span> <br />
           Reward your top readers
@@ -45,9 +49,20 @@ export const HeroSection: React.FC<HeroSectionProps> = ({}) => {
           </li>
         </ul>
       </header>
-      <div className="hidden md:flex justify-end">
-        <Image src={dashboard} alt="Screenshot of dashboard" quality={100} />
+      <div className="relative z-10 hidden md:flex justify-end md:w-5/6 lg:w-3/4 mx-auto">
+        <motion.div
+          animate={{ transformPerspective: 1000 }}
+          style={{ rotateX, scale }}
+        >
+          <Image
+            priority
+            src={dashboard}
+            alt="Screenshot of dashboard"
+            quality={100}
+          />
+        </motion.div>
       </div>
+      <div className="hidden md:block absolute bottom-0 z-0 h-96 w-full bg-gradient-to-b from-gray-100 to-gray-300" />
     </header>
   );
 };
