7f53dabe05838651c3fd8931e94e0e16832837f2
coderinblack08 <kevinlu.email@gmail.com>
2022-07-14 18:51:02 -0700

    fix: dialog portal

--- packages/ui/src/Modal.tsx
+++ packages/ui/src/Modal.tsx
@@ -43,65 +43,67 @@ export const Modal: React.VFC<ModalProps> = ({
   return (
     <Dialog.Root open={open} onOpenChange={handleOpenChange}>
       <Dialog.Trigger asChild>{trigger}</Dialog.Trigger>
-      <Transition show={open}>
-        <Dialog.Overlay forceMount>
-          <Transition.Child
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-            as={Fragment}
-          >
-            <div className="fixed inset-0 h-screen w-screen bg-black/75" />
-          </Transition.Child>
-        </Dialog.Overlay>
-        <Dialog.Content style={{ width: "100vw" }} forceMount>
-          <span
-            className="inline-block h-screen align-middle"
-            aria-hidden="true"
-          >
-            &#8203;
-          </span>
-          <Transition.Child
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
-            as={Fragment}
-          >
-            <div className="fixed z-50 inset-0">
-              <div
-                className="flex items-center justify-center min-h-screen p-4 text-center"
-                onClick={() => handleOpenChange(false)}
-              >
+      <Dialog.Portal forceMount>
+        <Transition show={open}>
+          <Dialog.Overlay forceMount>
+            <Transition.Child
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+              as={Fragment}
+            >
+              <div className="fixed inset-0 h-screen w-screen bg-black/75 z-50" />
+            </Transition.Child>
+          </Dialog.Overlay>
+          <Dialog.Content style={{ width: "100vw" }} forceMount>
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+              as={Fragment}
+            >
+              <div className="fixed z-50 inset-0">
                 <div
-                  className={`relative z-50 inline-block w-full max-w-lg overflow-hidden text-left align-middle transition-all transform bg-white rounded-2xl p-6 ${className}`}
-                  onClick={stopPropagation}
+                  className="flex items-center justify-center min-h-screen p-4 text-center"
+                  onClick={() => handleOpenChange(false)}
                 >
-                  <div className="pt-8">
-                    {children instanceof Function
-                      ? children({ open, setOpen })
-                      : children}
-                  </div>
-                  <button
-                    className="absolute top-0 right-0 m-5"
-                    onClick={() => handleOpenChange(false)}
+                  <div
+                    className={`relative z-50 inline-block w-full max-w-lg overflow-hidden text-left align-middle transition-all transform bg-white rounded-2xl p-6 ${className}`}
+                    onClick={stopPropagation}
                   >
-                    <IconX
-                      className="text-gray-500 focus:outline-none"
-                      size={16}
-                    />
-                  </button>
+                    <div className="pt-8">
+                      {children instanceof Function
+                        ? children({ open, setOpen })
+                        : children}
+                    </div>
+                    <button
+                      className="absolute top-0 right-0 m-5"
+                      onClick={() => handleOpenChange(false)}
+                    >
+                      <IconX
+                        className="text-gray-500 focus:outline-none"
+                        size={16}
+                      />
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Transition.Child>
-        </Dialog.Content>
-      </Transition>
+            </Transition.Child>
+          </Dialog.Content>
+        </Transition>
+      </Dialog.Portal>
     </Dialog.Root>
   );
 };
