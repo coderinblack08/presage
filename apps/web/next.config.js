5e643c20c17d02f0375362a9f415bf711a26f3a9
coderinblack08 <kevinlu.email@gmail.com>
2022-07-21 13:30:50 -0700

    fix: ueberdosis/tiptap/pull/2985

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,10 +1,15 @@
 const withTM = require("next-transpile-modules")(["ui"]);
 const { env } = require("./server/env");
 
-module.exports = withTM({
-  reactStrictMode: true,
-  publicRuntimeConfig: {
-    NODE_ENV: env.NODE_ENV,
-  },
-  experimental: { images: { allowFutureImage: true } },
-});
+module.exports = withTM(
+  /**
+   * @type {import('next').NextConfig}
+   */
+  {
+    reactStrictMode: true,
+    publicRuntimeConfig: {
+      NODE_ENV: env.NODE_ENV,
+    },
+    experimental: { images: { allowFutureImage: true, esmExternals: true } },
+  }
+);
